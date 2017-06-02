db1a870925fe35f92e950d83d1579efbf39c925a
ronneyramon <ronney@ymail.com>
2017-06-02 13:45:59 -0300

    Mais ajustes no script de deploy.

--- deploy.sh
+++ deploy.sh
@@ -124,7 +124,7 @@ fi
 # 4. Angular Prod Build
 if [ -e "$DEPLOYMENT_TARGET/.angular-cli.json" ]; then
   cd "$DEPLOYMENT_TARGET"
-  eval ./node_modules/.bin/ng build --dev
+  eval ./node_modules/.bin/ng build --prod
   exitWithMessageOnError "Angular build failed"
   cd - > /dev/null
 fi
